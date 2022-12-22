f466fdc077ce86b70d7f665427e8d96bcbf9cfc4
marufcse687 <marufcse687@gmail.com>
2022-12-22 16:34:48 +0600

    Initial Commit

---
+++ DepthEffect/DepthEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DepthEffectApp.swift
+//  DepthEffect
+//
+//  Created by Md Maruf Prodhan on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DepthEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
